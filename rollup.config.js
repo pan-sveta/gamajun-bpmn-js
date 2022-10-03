9b9140ff3a53ee59296c783292eaaa598ed3a219
Filip Štěpánek <stepafil@gmail.com>
2022-10-03 22:23:19 +0200

    Viewer added

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,35 @@ import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
-export default {
-  input: 'src/gamajun-modeler.js',
-  output: {
-    file: 'dist/gamajun-modeler.bundled.js',
-    name: 'CustomBpmnJS',
-    format: 'umd'
+export default [
+  {
+    input: 'src/gamajun-modeler.js',
+    output: {
+      file: 'dist/gamajun-modeler.bundled.js',
+      name: 'GamajunBpmnJS',
+      format: 'umd'
+    },
+    plugins: [
+      nodeResolve({
+        browser: true
+      }),
+      commonjs(),
+      json()
+    ]
   },
-  plugins: [
-    nodeResolve({
-      browser: true
-    }),
-    commonjs(),
-    json()
-  ]
-};
+  {
+    input: 'src/gamajun-viewer.js',
+    output: {
+      file: 'dist/gamajun-viewer.bundled.js',
+      name: 'GamajunBpmnJS',
+      format: 'umd'
+    },
+    plugins: [
+      nodeResolve({
+        browser: true
+      }),
+      commonjs(),
+      json()
+    ]
+  }
+];
